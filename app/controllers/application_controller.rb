3772012daced4a13199275aad8e53181e290b968
Sherry El Garhy <sherryptk@gmail.com>
2018-01-30 01:02:27 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,63 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  # HOME PAGE
   get '/' do
+
     erb :index
   end
 
+  # CREATE TWEET
+  get '/tweets/new' do
+
+    erb :'/tweets/create_tweet'
+  end
+
+  post '/tweets' do
+
+  end
+
+  # SHOW TWEET
+  get '/tweets/:id' do
+
+    erb :'/tweets/show_tweet'
+  end
+
+  # EDIT TWEET
+  get '/tweets/:id/edit' do
+
+    erb :'tweets/edit_tweet'
+  end
+
+  patch '/tweets/:id' do
+
+  end
+
+  # DELETE TWEET
+  delete '/tweets/:id/delete' do
+
+  end
+
+  # SIGN UP
   get '/signup' do
 
-    erb :create_user
+    erb :'/users/create_user'
+  end
+
+  post '/signup' do
+
+  end
+
+  # LOG IN
+  get '/login' do
+
+    erb :'/users/login'
+  end
+
+
+  get '/logout' do
+
+    erb :index
   end
 
 end
